c41527220882d9e0f820d723d647898766ecf3a5
Kevin Ye <yekevin1995@hotmail.com>
2018-12-08 13:28:04 -0500

    Move store to rootDir

---
+++ src/store.ts
@@ -0,0 +1,47 @@
+import createHistory from "history/createBrowserHistory"
+import { routerMiddleware } from "react-router-redux"
+import { applyMiddleware, compose, createStore } from "redux"
+import { persistReducer, persistStore } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import createSagaMiddleware from "redux-saga"
+
+import sagas from "./listeners"
+import rootReducer from "./redux"
+
+export const history = createHistory()
+
+export default () => {
+  const initialState = {}
+  const enhancers: any[] = []
+  const sagaMiddleware = createSagaMiddleware()
+
+  const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+  }
+
+  const middleware = [sagaMiddleware, routerMiddleware(history)]
+
+  if (process.env.NODE_ENV === "development") {
+    const devToolsExtension = (window as any).__REDUX_DEVTOOLS_EXTENSION__
+
+    if (typeof devToolsExtension === "function") {
+      enhancers.push(devToolsExtension())
+    }
+  }
+
+  const composedEnhancers = compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+  )
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+  const store = createStore(persistedReducer, initialState, composedEnhancers)
+
+  const persistor = persistStore(store)
+
+  sagaMiddleware.run(sagas)
+
+  return { store, persistor }
+}
